f91b8755a53f44511a05b2f5b936e2192df70521
ffernandez <ffernandez@app-con.com>
2018-06-11 18:08:13 -0300

    up

--- src/plugin.js
+++ src/plugin.js
@@ -12,6 +12,62 @@ const registerPlugin = videojs.registerPlugin || videojs.plugin;
 
 const PlayProgressBar = videojs.getComponent('PlayProgressBar');
 const MouseTimeDisplay = videojs.getComponent('MouseTimeDisplay');
+const LoadProgressBar = videojs.getComponent('LoadProgressBar');
+
+
+LoadProgressBar.prototype.update = function update(event) {
+  const buffered = this.player_.buffered();
+  const duration = this.player_.duration();
+  const bufferedEnd = this.player_.bufferedEnd();
+  const children = this.partEls_;
+
+  // get the percent width of a time compared to the total end
+  const percentify = function(time, end) {
+    // no NaN
+    let percent = (time / end) || 0;
+
+
+    if(percent < 0.001 || (end === Infinity && time !== 0)) percent = 1;
+
+    //percent = Math.round(percent * 100) / 100;
+
+    console.log({
+      time:time,
+      end:end,
+      percent:percent,
+      por:((percent >= 1 ? 1 : percent) * 100) + '%'
+    });
+
+    return ((percent >= 1 ? 1 : percent) * 100) + '%';
+  };
+
+
+
+  // update the width of the progress bar
+  this.el_.style.width = percentify(bufferedEnd, duration);
+
+  // add child elements to represent the individual buffered time ranges
+  for (let i = 0; i < buffered.length; i++) {
+    const start = buffered.start(i);
+    const end = buffered.end(i);
+    let part = children[i];
+
+    if (!part) {
+      part = this.el_.appendChild(document.createElement('div'));
+      children[i] = part;
+    }
+
+    // set the percent based on the width of the progress bar (bufferedEnd)
+    part.style.left = percentify(start, bufferedEnd);
+    part.style.width = percentify(end - start, bufferedEnd);
+  }
+
+  // remove unused buffered range elements
+  for (let i = children.length; i > buffered.length; i--) {
+    this.el_.removeChild(children[i - 1]);
+  }
+  children.length = buffered.length;
+}
 
 PlayProgressBar.prototype.update = function update(seekBarRect, seekBarPoint) {
 
@@ -25,9 +81,20 @@ PlayProgressBar.prototype.update = function update(seekBarRect, seekBarPoint) {
       this.player_.getCache().currentTime :
       this.player_.currentTime();
 
+
     const content = videojs.formatTime(this.player_.duration() - time, this.player_.duration());
-    //const content = videojs.formatTime(this.player_.duration() - time, this.player_.duration());
-    this.getChild('timeTooltip').update(seekBarRect, seekBarPoint, `-${content}`);
+
+
+    if(this.player_.duration() !== Infinity && !isNaN(this.player_.duration())){
+      /*
+      console.log(
+        "duration",this.player_.duration(),
+        "time",time,
+        "content",content);
+        */
+      //const content = videojs.formatTime(this.player_.duration() - time, this.player_.duration());
+      this.getChild('timeTooltip').update(seekBarRect, seekBarPoint, `-${content}`);
+    }
   });
 };
 MouseTimeDisplay.prototype.update = function update(seekBarRect, seekBarPoint) {
@@ -39,7 +106,7 @@ MouseTimeDisplay.prototype.update = function update(seekBarRect, seekBarPoint) {
 
   this.rafId_ = this.requestAnimationFrame(() => {
     const duration = this.player_.duration();
-    const content = videojs.formatTime(seekBarPoint * duration, duration);
+    //const content = videojs.formatTime(seekBarPoint * duration, duration);
     const content2 = videojs.formatTime(duration-(seekBarPoint * duration), duration);
     //console.log(seekBarRect, seekBarPoint,duration,content,content2);
     this.el_.style.left = `${seekBarRect.width * seekBarPoint}px`;
@@ -65,11 +132,10 @@ const onPlayerReady = (player, options) => {
 
   player.addClass('vjs-dvr');
 
-
-  player.controlBar.addClass('vjs-dvrseekbar-control-bar');
+  player.controlBar.addClass('vjs-dvr-control-bar');
 
   if (player.controlBar.progressControl) {
-    player.controlBar.progressControl.addClass('vjs-dvrseekbar-progress-control');
+    player.controlBar.progressControl.addClass('vjs-dvr-progress-control');
   }
 
   // ADD Live Button:
@@ -108,7 +174,7 @@ const onPlayerReady = (player, options) => {
 
   controlBar.insertBefore(btnLiveEl, insertBeforeNode);
 
-  videojs.log('dvrSeekbar Plugin ENABLED!', options);
+  //videojs.log('dvr Plugin ENABLED!', options);
 
 };
 
@@ -125,12 +191,13 @@ const onTimeUpdate = (player, e) => {
     return;
   }
 
+
+
   if (time.end(0) - player.currentTime() < 30) {
     btnLiveEl.className = 'vjs-live-label onair';
   } else {
     btnLiveEl.className = 'vjs-live-label';
   }
-
   player.duration(player.seekable().end(0));
 };
 
@@ -157,15 +224,11 @@ const dvr = function (options) {
     onTimeUpdate(this, e);
   });
 
-  this.on('play', (e) => {
-  });
-
   this.on('pause', (e) => {
     let btnLiveEl = document.getElementById('liveButton');
 
     btnLiveEl.className = 'vjs-live-label';
   });
-
 
   this.ready(() => {
     onPlayerReady(this, videojs.mergeOptions(defaults, options));
