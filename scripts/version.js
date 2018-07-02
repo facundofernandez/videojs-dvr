3221653ac0c9042dc79d9cbac5426c3a654a22f7
ffernandez <ffernandez@app-con.com>
2018-07-02 17:04:28 -0300

    update

--- scripts/version.js
+++ scripts/version.js
@@ -3,10 +3,9 @@ const path = require('path');
 const semver = require('semver');
 const pkg = require('../package.json');
 
-/*
+
 if (!semver.prerelease(pkg.version)) {
   process.chdir(path.resolve(__dirname, '..'));
   execSync('conventional-changelog -p videojs -i CHANGELOG.md -s');
   execSync('git add CHANGELOG.md');
 }
-*/
